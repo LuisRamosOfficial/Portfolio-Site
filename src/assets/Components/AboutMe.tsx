b661f0168bb368401abe4c95347e393a1852759e
Luis Ramos <120321858+LuisRamosOfficial@users.noreply.github.com>
2022-12-29 00:13:30 +0000

    Skills Done ✅

--- src/assets/Components/AboutMe.tsx
+++ src/assets/Components/AboutMe.tsx
@@ -1,8 +1,13 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import styles from '../styles/AboutMe.module.scss';
+import data from '../../data.json';
 
 const AboutMe: FC = () => {
 	const [Active, setActive] = useState<number>(0);
+	useEffect(() => {
+		console.log(data.Skills)
+		console.log(Object.keys(data.Skills))
+	}, [])
 
 	const clickHandler = (order: number) => {
 		setActive(order);
@@ -14,7 +19,7 @@ const AboutMe: FC = () => {
 				{Active == 0 && <AboutMeSection />}
 				{Active == 1 && <SkillsSection />}
 			</div>
-			<div>
+			<div className={styles.buttons}>
 				<Button
 					text={'About Me'}
 					order={0}
@@ -60,6 +65,16 @@ const SkillsSection = () => {
 	return (
 		<>
 			<h1>Skills</h1>
+			{Object.keys(data.Skills).map((e: string, index) => (
+				<div key={index}>
+					<h2 key={index}>{e}:</h2>
+					<span key={index + 1} className={styles.SkillSection}>
+						{data.Skills[e as keyof typeof data.Skills].map((el, index) => (
+							<img alt={el.Name} className={styles.skillImg} src={el.Icon} key={index} />
+						))}
+					</span>
+				</div>
+			))}
 		</>
 	);
 };
