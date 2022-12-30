356db12560f30aea489457af84c135275e3ab487
Luis Ramos <120321858+LuisRamosOfficial@users.noreply.github.com>
2022-12-30 22:38:57 +0000

    Added Controls to Project Section 🎮

--- src/assets/Components/ProjectSlider.tsx
+++ src/assets/Components/ProjectSlider.tsx
@@ -1,19 +1,64 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import styles from '../styles/ProjectSlider/ProjectSlider.module.css';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import data from '../../data.json';
-import demo from '../../../public/demo-button.png';
-import source from '../../../public/source-button.png';
+import demo from '../../demo-button.png';
+import source from '../../source-button.png';
+import nextArrow from '../../next-arrow.png';
+import prevArrow from '../../prev-arrow.png';
 
-const ProjectSlider = () => {
+interface UpdateSliderProps {
+	function :() => void
+
+}
+
+const ProjectSlider: FC = () => {
+
+	const [Widescreen, setWidescreen] = useState<boolean>(true);
+	const indexes = data.Projects.map((element, index) => {return index})
+
+	useEffect(() => {
+		const width = window.innerWidth;
+		console.log(width);
+		if (width < 1000) {
+			setWidescreen(false);
+		}
+	}, []);
+
+	const [currentSlider, setCurrentSlider] = useState<number>(0)
 
   const CarouselSettings = {
 		infiniteLoop: true,
-		width: '50%',
-		centerMode : false,
-    useKeyboardArrows: true
+		centerMode: false,
+		useKeyboardArrows: true,
+		showStatus: false,
+		swipeable: true,
+		emulateTouch: true,
+		showArrows: false,
+		showIndicators: false,
+		selectedItem: currentSlider,
 	};
+
+	const UpdateSlider = (to: string) => {
+		if (to == 'next') {
+			if (currentSlider == data.Projects.length - 1) {
+				setCurrentSlider(0)
+			}
+			else {
+				setCurrentSlider(currentSlider + 1)
+			}
+		}
+		else {
+			if (currentSlider == 0) {
+				setCurrentSlider(data.Projects.length - 1);
+			} else {
+				setCurrentSlider(currentSlider - 1);
+			}
+		}
+
+		console.log(indexes)
+	}
 
 	return (
 		<div className={styles.slider}>
@@ -22,8 +67,16 @@ const ProjectSlider = () => {
 				Here's a brief showcase of some projects with a Live Demo and the Source
 				Code.
 			</p>
+			<h5>
+				**Swipe, use the keyboard arrows or the displayed ones to switch
+				slides**
+			</h5>
+			<div className={styles.Controls}>
+				<img onClick={() => UpdateSlider('prev')} src={prevArrow} />
+				<img onClick={() => UpdateSlider('next')} src={nextArrow} />
+			</div>
 			<div className={styles.ProjectList}>
-				<Carousel {...CarouselSettings}>
+				<Carousel {...CarouselSettings} showThumbs={false} width={Widescreen ? '60%' : '90%'}>
 					{data.Projects.map((e, index) => (
 						<div key={index} className={styles.Project}>
 							<div
@@ -57,6 +110,9 @@ const ProjectSlider = () => {
 						</div>
 					))}
 				</Carousel>
+			</div>
+			<div className={styles.ControlDots}>
+				
 			</div>
 		</div>
 	);
