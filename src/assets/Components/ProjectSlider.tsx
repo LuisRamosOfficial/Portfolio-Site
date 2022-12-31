c30d3db372cdc8f92d125f19092558e196faa209
Luis Ramos <120321858+LuisRamosOfficial@users.noreply.github.com>
2022-12-31 10:44:34 +0000

    Projects List Done ✅

--- src/assets/Components/ProjectSlider.tsx
+++ src/assets/Components/ProjectSlider.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, useState, useEffect, useRef } from 'react';
 import styles from '../styles/ProjectSlider/ProjectSlider.module.css';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
@@ -9,26 +9,50 @@ import nextArrow from '../../next-arrow.png';
 import prevArrow from '../../prev-arrow.png';
 
 interface UpdateSliderProps {
-	function :() => void
-
+	function: () => void;
 }
 
-const ProjectSlider: FC = () => {
+let isMobile = true;
 
-	const [Widescreen, setWidescreen] = useState<boolean>(true);
-	const indexes = data.Projects.map((element, index) => {return index})
+const ProjectSlider: FC = () => {
+	const [width, setWidth] = useState(window.innerWidth);
+
+	const [intersecting, setIntersecting] = useState<boolean>(false);
+
+	const observer = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				setIntersecting(true);
+			} else {
+				setIntersecting(false);
+			}
+		});
+	});
+
+	const SliderRef = useRef(null);
+	useEffect(() => {
+		if (SliderRef.current) {
+			observer.observe(SliderRef.current);
+		}
+	}, [SliderRef]);
+
+	const handleWindowSizeChange = () => {
+		setWidth(window.innerWidth);
+		isMobile = window.innerWidth < 1200 ? true : false;
+	};
 
 	useEffect(() => {
-		const width = window.innerWidth;
-		console.log(width);
-		if (width < 1000) {
-			setWidescreen(false);
-		}
+		window.addEventListener('resize', handleWindowSizeChange);
+		handleWindowSizeChange();
 	}, []);
 
-	const [currentSlider, setCurrentSlider] = useState<number>(0)
+	const indexes = data.Projects.map((element, index) => {
+		return index;
+	});
 
-  const CarouselSettings = {
+	const [currentSlider, setCurrentSlider] = useState<number>(0);
+
+	const CarouselSettings = {
 		infiniteLoop: true,
 		centerMode: false,
 		useKeyboardArrows: true,
@@ -43,13 +67,11 @@ const ProjectSlider: FC = () => {
 	const UpdateSlider = (to: string) => {
 		if (to == 'next') {
 			if (currentSlider == data.Projects.length - 1) {
-				setCurrentSlider(0)
+				setCurrentSlider(0);
+			} else {
+				setCurrentSlider(currentSlider + 1);
 			}
-			else {
-				setCurrentSlider(currentSlider + 1)
-			}
-		}
-		else {
+		} else {
 			if (currentSlider == 0) {
 				setCurrentSlider(data.Projects.length - 1);
 			} else {
@@ -57,65 +79,79 @@ const ProjectSlider: FC = () => {
 			}
 		}
 
-		console.log(indexes)
-	}
+		console.log(indexes);
+	};
 
 	return (
-		<div className={styles.slider}>
-			<h1>My Projects</h1>
-			<p>
-				Here's a brief showcase of some projects with a Live Demo and the Source
-				Code.
-			</p>
-			<h5>
-				**Swipe, use the keyboard arrows or the displayed ones to switch
-				slides**
-			</h5>
-			<div className={styles.Controls}>
-				<img onClick={() => UpdateSlider('prev')} src={prevArrow} />
-				<img onClick={() => UpdateSlider('next')} src={nextArrow} />
-			</div>
-			<div className={styles.ProjectList}>
-				<Carousel {...CarouselSettings} showThumbs={false} width={Widescreen ? '60%' : '90%'}>
-					{data.Projects.map((e, index) => (
-						<div key={index} className={styles.Project}>
-							<div
-								className={styles.background}
-								style={{ backgroundImage: `url(${e.ScreenShot})` }}
-							></div>
-							<div className={styles.titlebar}>
-								<h2>{e.Name}</h2>
-							</div>
-							<div className={styles.footbar}>
-								<h4>{e.Description}</h4>
-								<div className={styles.buttons}>
-									<button
-										onClick={() => {
-											window.open(e.Demo, '_blank');
-										}}
-										className={styles.DemoButton}
-									>
-										<img alt="Live Demo" src={demo} />
-									</button>
-									<button
-										onClick={() => {
-											window.open(e.Github, '_blank');
-										}}
-										className={styles.SourceButton}
-									>
-										<img alt="Source Code" src={source} />
-									</button>
+			<div className={styles.slider} ref={SliderRef}>
+				<h1>My Projects</h1>
+				<p>
+					Here's a brief showcase of some projects with a Live Demo and the
+					Source Code.
+				</p>
+				<h5>
+					**Swipe, use the keyboard arrows or the displayed ones to switch
+					slides**
+				</h5>
+				<div className={styles.Controls}>
+					<img onClick={() => UpdateSlider('prev')} src={prevArrow} />
+					<img onClick={() => UpdateSlider('next')} src={nextArrow} />
+				</div>
+				<div className={styles.ProjectList}>
+					<Carousel
+						{...CarouselSettings}
+						showThumbs={false}
+						width={isMobile ? '90%' : '60%'}
+					>
+						{data.Projects.map((e, index) => (
+							<div key={index} className={styles.Project}>
+								<div
+									className={styles.background}
+									style={{ backgroundImage: `url(${e.ScreenShot})` }}
+								></div>
+								<div className={styles.titlebar}>
+									<h2>{e.Name}</h2>
+								</div>
+								<div className={styles.footbar}>
+									<h4>{e.Description}</h4>
+									<div className={styles.buttons}>
+										<button
+											onClick={() => {
+												window.open(e.Demo, '_blank');
+											}}
+											className={styles.DemoButton}
+										>
+											<img alt="Live Demo" src={demo} />
+										</button>
+										<button
+											onClick={() => {
+												window.open(e.Github, '_blank');
+											}}
+											className={styles.SourceButton}
+										>
+											<img alt="Source Code" src={source} />
+										</button>
+									</div>
 								</div>
 							</div>
-						</div>
+						))}
+					</Carousel>
+				</div>
+				<div className={styles.ControlDots}>
+					{indexes.map((element) => (
+						<>
+							{currentSlider == element ? (
+								<p>&#9679;</p>
+							) : (
+								<p onClick={() => setCurrentSlider(element)}>&#9675;</p>
+							)}
+						</>
 					))}
-				</Carousel>
+				</div>
 			</div>
-			<div className={styles.ControlDots}>
-				
-			</div>
-		</div>
+
 	);
 };
+
 
 export default ProjectSlider;
